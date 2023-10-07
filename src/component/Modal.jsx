8cfb22ef2aa9f9df9aed61501fafdaff38da0f64
betelhem <betelhemkirub@gmail.com>
2023-10-07 17:13:09 +0300

    edited modal

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -21,7 +21,7 @@ const Modal = ({ onClose, children, actionBar }) => {
   return (
    <div>
       <div onClick={onClose} className="z-50 fixed overflow-scroll bg-black/70 top-0 left-0 right-0 bottom-0"></div>
-      <div className="fixed z-50 w-[90vw] h-[90vw] md:w-[60vw] md:h-[80vh] top-[50%] -translate-x-1/2 -translate-y-1/2 right-1/2 bottom-1/2 left-1/2 bg-white p-6 lg:p-12 overflow-y-auto">
+      <div className="fixed z-50 w-[90vw] h-[100vw] md:w-[60vw] md:h-[80vh] top-[50%] -translate-x-1/2 -translate-y-1/2 right-1/2 bottom-1/2 left-1/2 bg-white p-6 lg:p-12 overflow-y-auto">
         <div className="">{children}</div>
         {actionBar}
       </div>
